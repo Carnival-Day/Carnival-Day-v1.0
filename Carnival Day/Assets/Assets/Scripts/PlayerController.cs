35e76f05ba09b47d1e22235a6cf6f7778dfbb094
vanessa1350 <80784339+Vanessa1350@users.noreply.github.com>
2024-01-31 12:12:30 -0800

    Name change

--- Carnival Day/Assets/Assets/Scripts/PlayerController.cs
+++ Carnival Day/Assets/Assets/Scripts/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
         if (playerFromMongo == null) {
             // Create playerdata when the game is running to define some variable
             _playerData = new PlayerData();
-            _playerData.aName = "Phyllie";
+            _playerData.aName = "Laika";
             _playerData.aValue = 0;
         } else {
             // PlayerData as PlayerfromMongo
