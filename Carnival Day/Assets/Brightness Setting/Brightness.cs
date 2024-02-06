897990d580beae54ec0753d36c50340b7e1e1d69
trietnguyen456 <triet.nguyen@student.csulb.edu>
2024-02-06 01:59:18 -0800

    brightness slider
    -Implemented brightness slider

---
+++ Carnival Day/Assets/Brightness Setting/Brightness.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Brightness : MonoBehaviour
+{
+    public Slider brightnessSlider;
+    public PostProcessProfile brightness;
+    public PostProcessLayer layer;
+
+    private AutoExposure exposure;
+    // Start is called before the first frame update
+
+    //Match the brightness with the slider value when game start
+    void Start()
+    {
+        brightness.TryGetSettings(out exposure);
+        AdjustBrightness(brightnessSlider.value);
+    }
+
+    //Set exposure value to the value of brightnessSlider
+    //Set minimum brightness to 0.05 so it won't black out
+    public void AdjustBrightness(float value)
+    {
+        if(value != 0)
+        {
+            exposure.keyValue.value = value;
+        }
+        else
+        {
+            exposure.keyValue.value = 0.05f;
+        }
+    }
+}
