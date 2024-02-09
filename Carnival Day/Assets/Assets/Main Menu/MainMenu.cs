f858f62712a76dd64fd3a358ee7d704c52e9657f
andrew-t-nguyen <andrew.thienlong.nguyen@gmail.com>
2024-02-08 16:07:44 -0800

    derick jhelp

---
+++ Carnival Day/Assets/Assets/Main Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
